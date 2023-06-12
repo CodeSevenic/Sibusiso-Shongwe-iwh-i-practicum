21c3ba1c55b472ea198472b3c084c6ade52ea2e1
CodeSevenic <sevenicbookings@gmail.com>
2023-06-12 13:46:05 +0200

    Created Endpoint for get custom object data, Update form and last for the custom objects form to create or update your custom object data.

--- index.js
+++ index.js
@@ -8,19 +8,63 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // * Please include the private app access token in your repo BUT only an access token built in a TEST ACCOUNT. Don't do this practicum in your normal account.
-const PRIVATE_APP_ACCESS = '';
+const PRIVATE_APP_ACCESS = 'pat-eu1-9b9c5964-c226-4c3b-9d85-f6ee952b44d7';
 
 // TODO: ROUTE 1 - Create a new app.get route for the homepage to call your custom object data. Pass this data along to the front-end and create a new pug template in the views folder.
+app.get('/', async (req, res) => {
+  const properties = ['director', 'movie_name', 'main_actor'];
 
-// * Code for Route 1 goes here
+  const endpoint = `https://api.hubspot.com/crm/v3/objects/2-115072281?properties=${properties.join(
+    ','
+  )}`;
+
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json',
+  };
+  try {
+    const resp = await axios.get(endpoint, { headers });
+    const data = resp.data.results;
+    for (let i = 0; i < data.length; i++) {
+      console.log(data[i].properties);
+    }
+    res.render('homepage', { title: 'Homepage | HubSpot APIs', data });
+  } catch (error) {
+    console.error(error);
+  }
+});
 
 // TODO: ROUTE 2 - Create a new app.get route for the form to create or update new custom object data. Send this data along in the next route.
-
-// * Code for Route 2 goes here
+app.get('/update-cobj', (req, res) => {
+  res.render('updates', {
+    title: 'Update Custom Object Form | Integrating With HubSpot | Practicum',
+  });
+});
 
 // TODO: ROUTE 3 - Create a new app.post route for the custom objects form to create or update your custom object data. Once executed, redirect the user to the homepage.
+app.post('/update-cobj', async (req, res) => {
+  const data = {
+    properties: {
+      director: req.body.director,
+      movie_name: req.body.movie_name,
+      main_actor: req.body.main_actor,
+    },
+  };
 
-// * Code for Route 3 goes here
+  const endpoint = 'https://api.hubspot.com/crm/v3/objects/2-115072281';
+
+  const headers = {
+    Authorization: `Bearer ${PRIVATE_APP_ACCESS}`,
+    'Content-Type': 'application/json',
+  };
+
+  try {
+    await axios.post(endpoint, data, { headers });
+    res.redirect('/');
+  } catch (err) {
+    console.error(err);
+  }
+});
 
 /** 
 * * This is sample code to give you a reference for how you should structure your calls. 
@@ -65,7 +109,6 @@ app.post('/update', async (req, res) => {
 
 });
 */
-
 
 // * Localhost
 app.listen(3000, () => console.log('Listening on http://localhost:3000'));
